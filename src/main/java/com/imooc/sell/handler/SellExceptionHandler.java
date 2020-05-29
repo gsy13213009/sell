01a36fdbd496db3f970855cff8432ea30f4d9111
Siyi Guo <siyi.guo@grabtaxi.com>
2020-05-29 10:29:00 +0800

    (CHORE) 新增SellException的统一处理，返回固定格式，修改返回的状态码

--- src/main/java/com/imooc/sell/handler/SellExceptionHandler.java
+++ src/main/java/com/imooc/sell/handler/SellExceptionHandler.java
@@ -1,10 +1,17 @@
 package com.imooc.sell.handler;
 
 import com.imooc.sell.config.ProjectUrlConfig;
+import com.imooc.sell.exception.SellException;
 import com.imooc.sell.exception.SellerAuthorizeException;
+import com.imooc.sell.utils.ResultVOUtil;
+import com.imooc.sell.vo.ResultVO;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
@@ -16,6 +23,7 @@ public class SellExceptionHandler {
     //拦截登录异常
     //http://sell.natapp4.cc/sell/wechat/qrAuthorize?returnUrl=http://sell.natapp4.cc/sell/seller/login
     @ExceptionHandler(value = SellerAuthorizeException.class)
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
     public ModelAndView handlerAuthorizeException() {
 //        return new ModelAndView("redirect:"
 //            .concat(projectUrlConfig.getWechatOpenAuthorize())
@@ -26,5 +34,15 @@ public class SellExceptionHandler {
         return new ModelAndView("redirect:"
             .concat(projectUrlConfig.getSell())
             .concat("/sell/seller/login"));
+    }
+
+    // 拦截SellException异常，比如创建订单时，商品不存在等
+    @ExceptionHandler(value = SellException.class)
+    @ResponseBody
+    // 该异常返回的status
+    @ResponseStatus(value = HttpStatus.BAD_GATEWAY) // 可以多创建几个异常类型，每个异常类型返回不同的code
+    public ResultVO handlerSellerException(SellException e, HttpServletResponse response) {
+//        response.setStatus(500);// 也可以使用该方式修改返回的code
+        return ResultVOUtil.error(e.getCode(), e.getMessage());
     }
 }
